b80ff4eb45a6f5e050469fdd81f2f9d1d92e983f
Prakash Dodawad <prakash@mobil80.com>
2025-08-18 15:45:17 +0530

    Added youtube link

---
+++ lib/data/portfolio_data.dart
@@ -0,0 +1,174 @@
+const Map<String, dynamic> portfolioData = {
+  "name": "Prakash Dodawad",
+  "tagline": "Building engaging, cross-platform apps with Flutter.",
+  "professionalSummary":
+      "Experienced Flutter Developer with over a year of creating high-quality, cross-platform apps. Skilled at translating complex requirements into engaging user experiences and delivering reliable solutions in fast-paced environments. Committed to continuous learning and innovation. I have a strong foundation in Flutter development, including state management, API integration, and performance optimization. My experience spans the full app lifecycle, from initial design and development to deployment and maintenance.",
+  "contact": {
+    "email": "prakashgdodawad@gmail.com",
+    "phone": "+916366133365",
+    "resume":
+        "https://drive.google.com/file/d/10T55Kjx_zAFNTGepaP8DNX2Q9MtMWcEj/view?usp=sharing",
+    "linkedin": "https://www.linkedin.com/in/prakash-dodawad-a621571b8",
+    "github": "https://github.com/PrakashDodawad2024",
+    "youtube": "https://www.youtube.com/@FlutterbyPrakash",
+    "location": "Bangalore, Karnataka, Pin code 560097",
+    "dob": "23rd May 2000"
+  },
+  "skills": {
+    "languages": ["Flutter", "Dart", "Core Java"],
+    "databases": ["Hive Database", "MySQL"],
+    "api": ["Rest", "GraphQL", "MQTT"],
+    "stateManagement": ["GetX", "Provider", "BLOC", "MVC", "MVVM"],
+    "tools": ["Visual Studio Code", "Android Studio", "Xcode", "GitHub"]
+  },
+  "workExperience": [
+    {
+      "company": "Mobil80 Solution and Services Pvt Limited",
+      "role": "Software Engineer (Flutter Developer)",
+      "duration": "02nd November 2022 – Present",
+      "responsibilities": [
+        "Developed and implemented user-friendly mobile applications using Flutter.",
+        "Collaborated with senior developers and designers to integrate UI/UX designs and third-party libraries.",
+        "Integrated APIs and services, conducted unit tests, and fixed bugs to ensure app stability and performance.",
+        "Researched and added new features and functionalities to enhance Flutter applications."
+      ]
+    }
+  ],
+  "projects": [
+    {
+      "name": "ServiceWRK Technician",
+      "description":
+          "A mobile app to manage service tickets and increase productivity.",
+      "imageUrl": "assets/images/servicewrk.png",
+      "details": [
+        "Utilized Firebase Auth for secure user authentication and login.",
+        "Integrated Google Maps to provide location services and track technicians.",
+        "Managed data efficiently with GraphQL API and Hive for local storage.",
+        "Implemented GetX for robust state management.",
+        "Incorporated Shake Detector and Screenshot features for enhanced interactivity."
+      ],
+      "links": [
+        {
+          "type": "Play Store",
+          "url":
+              "https://play.google.com/store/apps/details?id=com.mobil80.servicewrk"
+        },
+        {
+          "type": "App Store",
+          "url": "https://apps.apple.com/in/app/servicewrk/id1609190294"
+        }
+      ]
+    },
+    {
+      "name": "ServiceWRK Agent",
+      "description":
+          "A back-end service agent app for creating, assigning, and monitoring tickets from anywhere.",
+      "imageUrl": "assets/images/servicewrkagent.png",
+      "details": [
+        "Enabled agents to create, assign, and manage service tickets on the go.",
+        "Provided real-time monitoring of ticket status and technician locations.",
+        "Integrated Google Maps to provide location services and track agent.",
+        "Managed data efficiently with GraphQL API and Hive for local storage.",
+        "Implemented GetX for robust state management.",
+      ],
+      "links": [
+        {
+          "type": "Play Store",
+          "url":
+              "https://play.google.com/store/apps/details?id=com.mobil80.servicewrkagent"
+        },
+        {
+          "type": "App Store",
+          "url": "https://apps.apple.com/in/app/servicewrk-agent/id1612823765"
+        }
+      ]
+    },
+    {
+      "name": "PharmaWRK",
+      "description":
+          "An easy tool to assist Medical Representatives in the field.",
+      "imageUrl": "assets/images/pharmawrk.png",
+      "details": [
+        "Features Firebase Auth and Google Maps for user authentication and location services.",
+        "Uses GraphQL API and Hive database for efficient and offline-friendly data handling.",
+        "Leveraged the GetX state management package for a responsive and scalable architecture.",
+        "Enabled uploading and fetching files from AWS S3, along with handling dynamic form fields.",
+        "Implemented Hive for submitting offline visit logs reliably.",
+        "Designed to be fully responsive across both mobile devices and iPads.",
+      ],
+      "links": [
+        {
+          "type": "Play Store",
+          "url":
+              "https://play.google.com/store/apps/details?id=com.mobil80.pharmawrk"
+        },
+        {
+          "type": "App Store",
+          "url": "https://apps.apple.com/in/app/pharmawrk/id1575723537"
+        }
+      ]
+    },
+    {
+      "name": "Dyrectori",
+      "description":
+          "Organize & share contact books, directories, and lists with others or use solo.",
+      "imageUrl": "assets/images/dyrectori.png",
+      "details": [
+        "Created dynamic directories with customizable fields for flexible data input",
+        "Displayed text fields dynamically while adding entries based on directory structure",
+        "Integrated amplify_storage_s3 to upload and manage files on AWS S3",
+        "Displayed user and directory locations using google_maps_flutter",
+        "Fetched directory lists from AWS S3 using aws_client for real-time access",
+        "Used hive_flutter to store and display directories for offline access",
+        "Implemented razorpay_flutter on Android and RevenueCat on iOS for directory upgrades",
+      ],
+      "links": [
+        {
+          "type": "Play Store",
+          "url":
+              "https://play.google.com/store/apps/details?id=com.mobil80.dyrectori&pcampaignid=web_share"
+        },
+        {
+          "type": "App Store",
+          "url":
+              "https://apps.apple.com/in/app/dyrectori-shared-entry-books/id6742438975"
+        }
+      ]
+    },
+    {
+      "name": "Radio.ONE",
+      "description":
+          "Enjoy a world of music at your fingertips with Radio.ONE app!.",
+      "imageUrl": "assets/images/radioone.png",
+      "details": [
+        "Tune in to your favorite stations across genres - relax, dance, or explore new sounds",
+        "Set your favorite stations for instant access anytime",
+        "Used radio_player package to stream live radio stations seamlessly",
+        "Implemented MQTT protocol to list and update all available radio stations in real time",
+        "Utilized Hive database to store radio station data for offline access and fast loading",
+      ],
+      "links": [
+        {"type": "Play Store", "url": ""},
+        {"type": "App Store", "url": ""}
+      ]
+    }
+  ],
+  "education": [
+    {
+      "school":
+          "KLS’s Vishwanathrao Deshpande Institute of Technology, Haliyal",
+      "degree": "B. E in Civil Engineering",
+      "duration": "2017-2021",
+    },
+    {
+      "school": "Madeena Pre-University College, Dharwad",
+      "degree": "Pre-University Course",
+      "duration": "2016-2017",
+    },
+    {
+      "school": "Basel Mission Boys High School, Dharwad",
+      "degree": "Secondary School Leaving Certificate",
+      "duration": "2014-2015",
+    }
+  ]
+};
